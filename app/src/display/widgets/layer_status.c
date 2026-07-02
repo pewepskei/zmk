696ac6c66e8c27fc2328a95857fb24f506185996
Philip <philip.d.dumoran@gmail.com>
2026-07-03 00:02:23 +0800

    Update layer_status.c

--- app/src/display/widgets/layer_status.c
+++ app/src/display/widgets/layer_status.c
@@ -26,7 +26,7 @@ static void set_layer_symbol(lv_obj_t *label, struct layer_status_state state) {
     if (state.label == NULL || strlen(state.label) == 0) {
         char text[8] = {};
 
-        snprintf(text, sizeof(text), LV_SYMBOL_KEYBOARD " %i", state.index);
+        snprintf(text, sizeof(text), "%i", state.index);
 
         lv_label_set_text(label, text);
     } else {
